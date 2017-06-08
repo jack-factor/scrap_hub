35c8a32e10ec6436ca7646ed48c761b275397096
Jack Moreno <jack12972@gmail.com>
2017-06-08 16:49:34 -0500

    extract index information

---
+++ scrapgh.py
@@ -0,0 +1,72 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+USER_GH = 'jack-factor'
+BASE_URL = 'https://github.com/'
+
+
+def get_obj_current_page(user):
+    try:
+        gh_html = urlopen(BASE_URL + user)
+        bsObj = BeautifulSoup(gh_html.read(), 'html.parser')
+        return bsObj
+    except Exception:
+        return None
+
+
+def get_index_data(user=USER_GH):
+    pageObj = get_obj_current_page(user)
+    if pageObj is None:
+        return None
+    data = {}
+    # data profile
+    mainContent = pageObj.find('div', {'role': 'main'})
+    name = mainContent.find('span', {'class': 'vcard-fullname'})
+    username = mainContent.find('span', {'class': 'vcard-username'})
+    html_bio = mainContent.find('div', {'class': 'user-profile-bio'})
+    image = mainContent.find('img', {'class': 'avatar'})['src']
+
+    # description
+    description = ''
+    if html_bio is not None:
+        description = html_bio.text
+    # organization
+    organization = pageObj.findAll('a', {'class': 'avatar-group-item'})
+    org_data = []
+    for item in organization:
+        org_data.append({'href': item['href'],
+                         'name': item['aria-label']})
+    data['organization'] = org_data
+    data['name'] = name.text
+    data['image'] = image
+    data['username'] = username.text
+    data['description'] = description
+    # repositories
+    repositories = pageObj.findAll('li', {'class': 'pinned-repo-item'})
+    repo_data = []
+    for repository in repositories:
+        rep_text = repository.find('a')
+        rep_body = repository.find('span',
+                                   {'class': 'pinned-repo-item-content'})
+        # language
+        text_language = ''
+        if rep_body.find('span', {'class': 'repo-language-color'}) is not None:
+            rep_body.find('span', {'class': 'repo-language-color'}).extract()
+            [a.extract() for a in rep_body.findAll('a')]
+            text_language = rep_body.find(
+                'p', {'class': 'mb-0'}
+            ).text.strip('\n ')
+
+        repo_data.append({
+            'title': rep_text.find('span').text,
+            'description': rep_body.find(
+                'p', {'class': 'pinned-repo-desc'}
+            ).text,
+            'url': rep_text['href'],
+            'langauage': text_language})
+    data['repositories'] = repo_data
+    return data
+
+
+pageObj = get_index_data()
+print(pageObj)
